2d787ccc08ebe3ba6edd89aa6a5730473fb322bf
Josephine Chong <josephine.chong@myob.com>
2021-10-25 16:27:41 +1100

    Created List for Dice in Game Dice

--- Yatzy/GameDice.cs
+++ Yatzy/GameDice.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
+
 namespace Yatzy
 {
-    public class GameDice
+    public class GameDice //roll or hold & change class Dice to Die (singluar)
     {
+        public List<Dice> Dice; 
+        public GameDice()
+        {
+            Dice = new List<Dice>(
+                
+                );
+        }
         //Dice 1
         //Dice 2
         //Dice 3
         //Dice 4
         //Dice 5
-        
     }
 }
