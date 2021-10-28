86482dc8202be3fdf01987b7bcdb9ba891513a05
Josephine Chong <josephine.chong@myob.com>
2021-10-28 16:22:53 +1100

    Added a convienence method to game dice test for checking that re rolled dice have a different face value

--- Yatzy/GameDice.cs
+++ Yatzy/GameDice.cs
@@ -5,6 +5,7 @@ namespace Yatzy
     public class GameDice //roll or hold
     {
         public List<Die> Dice;
+        private List<Die> _finalSelection;
   
         public GameDice(IRandomNumberGenerator randomNumberGenerator)
         {
@@ -24,16 +25,49 @@ namespace Yatzy
 
         public void HoldDice(List<int> playersHeldDice) //won't work for if they only want to keep just 1x 1's (if they have 3) 
         {
-            for (var i = 0; i < Dice.Count; i++)
+            // 1, 1, 5, 6, 3
+            
+            //hold 1, 3, 5
+            
+            //new list for only the dice list with the players selected choice
+
+            //_finalSelection = playersHeldDice; // instead of bool 
+            //after validation
+            
+
+            //List All (5) - Player's choice (1-5)
+
+            //validation for if what they have chosen is a valid pick/in the existing Dice list
+
+            // for (var i = 0; i < Dice.Count; i++)
+            // {
+            //     for (var j = 0; j < playersHeldDice.Count; j++)
+            //     {
+            //         if (playersHeldDice[j] == Dice[i].Face)
+            //         {
+            //             Dice[i].Hold();
+            //         }
+            //     }
+            // }
+            
+            //Dice 1 = index 0
+
+            for (var i = 0; i < playersHeldDice.Count; i++)
             {
-                for (var j = 0; j < playersHeldDice.Count; j++)
-                {
-                    if (playersHeldDice[j] == Dice[i].Face)
-                    {
-                        Dice[i].Hold();
-                    }
-                }
+                Dice[playersHeldDice[i]].Hold();
             }
+            //using the dice index 
+             // for (var i = 0; i < Dice.Count; i++)
+             // {
+             //     for (var j = 0; j < playersHeldDice.Count; j++)
+             //     {
+             //         //Dice 1 = index 0
+             //         if (j - 1 == i)
+             //         {
+             //             Dice[i].Hold();
+             //         }
+             //     }
+             // }
         }
     }
 }
--- YatzyTest/GameDiceTest.cs
+++ YatzyTest/GameDiceTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Moq;
 using Xunit;
 using Yatzy;
@@ -107,6 +108,61 @@ namespace YatzyTest
             Assert.Equal(thirdHeldDieFace, thirdHeldDieFacePostRoll);
             Assert.NotEqual(firstNonHeldDieFace, firstNonHeldDieFacePostRoll);
             Assert.NotEqual(secondNonHeldDieFace, secondNonHeldDieFacePostRoll);
+        }
+        
+        [Fact]
+        public void Player_Should_Be_Able_To_Select_Specific_Amount_Of_The_Same_Face_Number_To_Be_Held()
+        {
+            //arrange
+            var mockRandomNumberGenerator = new Mock<IRandomNumberGenerator>();
+            mockRandomNumberGenerator.SetupSequence(m => m.RandomNumber(1, 6))
+                .Returns(1) //dice 1
+                .Returns(1) //dice 2
+                .Returns(3) //dice 3
+                .Returns(1) //dice 4
+                .Returns(5) //dice 5
+                .Returns(2) //re roll for dice 4
+                .Returns(4); //re roll for dice 5
+
+            var gameDice = new GameDice(mockRandomNumberGenerator.Object);
+            var mockPlayerHeldList = new List<int>() {0, 1, 2}; //Dice index
+            
+            //act
+            gameDice.RollDice();
+            var diceAfterFirstRoll = gameDice.Dice.Select(d => d.Face).ToList();
+            
+            gameDice.HoldDice(mockPlayerHeldList);
+            
+            gameDice.RollDice();
+            var diceAfterSecondRoll = gameDice.Dice;
+            
+            //assert
+            Assert.True(gameDice.Dice[0].IsHeld());
+            Assert.True(gameDice.Dice[1].IsHeld());
+            Assert.True(gameDice.Dice[2].IsHeld());
+            Assert.False(gameDice.Dice[3].IsHeld());
+            Assert.False(gameDice.Dice[4].IsHeld());
+
+            Dice_Should_Reroll_Unless_Held(diceAfterFirstRoll, diceAfterSecondRoll);
+
+        }
+
+        private void Dice_Should_Reroll_Unless_Held(List<int> diceAfterFirstRoll, List<Die> diceAfterSecondRoll)
+        {
+            for (var i = 0; i < diceAfterFirstRoll.Count; i++)
+            {
+                var dieAfterFirstRoll = diceAfterFirstRoll[i];
+                var dieAfterSecondRoll = diceAfterSecondRoll[i];
+                
+                if (dieAfterSecondRoll.IsHeld())
+                {
+                    Assert.Equal(dieAfterFirstRoll, dieAfterSecondRoll.Face);
+                }
+                else
+                {
+                    Assert.NotEqual(dieAfterFirstRoll, dieAfterSecondRoll.Face);
+                }
+            }  
         }
     }
 }
