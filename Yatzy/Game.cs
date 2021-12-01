763dbcccd3d348f0dab2f92333834165a1cb1c61
Josephine Chong <josephine.chong@myob.com>
2021-12-01 16:53:01 +1100

    refactored reset function for game class, test is passing

--- Yatzy/Game.cs
+++ Yatzy/Game.cs
@@ -11,12 +11,12 @@ namespace Yatzy
         //players should be in a list or array and loop through 
         private readonly IConsole _console;
         public GameDice GameDice;
-        public Dictionary<string, int> ScoreRecords;
+        public Dictionary<string, List<int>> ScoreRecords; // 
         public Game(IConsole console, IRandomNumberGenerator randomNumberGenerator)
         {
             _console = console;
             GameDice = new GameDice(randomNumberGenerator, console);
-            ScoreRecords = new Dictionary<string, int>();
+            ScoreRecords = new Dictionary<string, List<int>>();
         }
         
         public void Run()
@@ -24,24 +24,40 @@ namespace Yatzy
             _console.WriteLine("Welcome to Yatzy. \nWhat is your name?");
             var playerName = _console.ReadLine(); 
             var player = new Player(_console, playerName);
-            var loop = true;
+            
             
             while (GameShouldContinue(player)) //
             {
-                if (AllCategoriesHaveBeenPlayed(player) && PLayerWantsToResetGame()) //if we get here and all categories have been played, player wants to reset
+                //if cat game won == 10 Print dinal score
+                
+                if (AllCategoriesHaveBeenPlayed(player)) //if we get here and all categories have been played, player wants to reset
                 {
+                    if (ScoreRecords.ContainsKey(playerName))
+                    {
+                        ScoreRecords[playerName].Add(player.Score);
+                    }
+                    else
+                    {
+                        ScoreRecords.Add(playerName, new List<int>{player.Score});
+                    }
+                    
                     player = player.Reset();
                 }
-                // else
-                // {
-                //     loop = false;
-                // }
                 
                 PlayerRollsDice(player);
                 PlayerChoosesCategory(player); 
+                
             }
-            
-            ScoreRecords.Add(playerName, player.Score); //completed game or player has quit the game and that's their final score
+            if (ScoreRecords.ContainsKey(playerName))
+            {
+                ScoreRecords[playerName].Add(player.Score);
+            }
+            else
+            {
+                ScoreRecords.Add(playerName, new List<int>{player.Score});
+            }
+           
+            //completed game or player has quit the game and that's their final score
             PrintPlayersScores();
             
             //reset game
@@ -54,7 +70,7 @@ namespace Yatzy
             _console.WriteLine("Congratulations on finishing your Yatzy game. Here are the results: ");
             foreach (var record in ScoreRecords)
             {
-                _console.WriteLine($"{record.Key}'s final score is {record.Value}. ");
+                _console.WriteLine($"{record.Key}'s final score is {record.Value.Sum()}. ");
             }
         }
 
