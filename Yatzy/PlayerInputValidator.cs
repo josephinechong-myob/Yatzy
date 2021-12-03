e62036f00f6cff4e8bb19399a6cd33be12fbf275
Josephine Chong <josephine.chong@myob.com>
2021-12-03 11:02:39 +1100

    refactor Player input validator class

--- Yatzy/PlayerInputValidator.cs
+++ Yatzy/PlayerInputValidator.cs
@@ -10,28 +10,28 @@ namespace Yatzy
         {
             _console = console;
         }
-        
-        public bool StringIsOnlyNumbers(string playerInput) //cat no
+
+        private bool IsValidInput(string playerInput, Regex validPattern)
+        {
+            var stringIsNotEmpty = playerInput != string.Empty;
+            var patternIsMatch = validPattern.IsMatch(playerInput);
+            return stringIsNotEmpty && patternIsMatch; 
+        }
+        public bool StringIsOnlyNumbers(string playerInput) //function that returns a boolean that has all 4 lines
         {
             var validPattern = new Regex("^[1-9][0]?$");
-            var stringIsNotEmpty = playerInput != string.Empty;
-            var patternIsMatch = validPattern.IsMatch(playerInput);
-            return stringIsNotEmpty && patternIsMatch;
+            return IsValidInput(playerInput, validPattern);
         }
         
-        public bool StringIsOnlyNumbersOneToSix(string playerInput) //spec no select
+        public bool StringIsOnlyNumbersOneToSix(string playerInput)
         {
             var validPattern = new Regex("^[1-6]$");
-            var stringIsNotEmpty = playerInput != string.Empty;
-            var patternIsMatch = validPattern.IsMatch(playerInput);
-            return stringIsNotEmpty && patternIsMatch;
+            return IsValidInput(playerInput, validPattern);
         }
         public bool ResponseIsYOrN(string playerInput) 
         {
             var validPattern = new Regex("^[YN]$");
-            var stringIsNotEmpty = playerInput != string.Empty;
-            var patternIsMatch = validPattern.IsMatch(playerInput);
-            return stringIsNotEmpty && patternIsMatch;
+            return IsValidInput(playerInput, validPattern);
         }
     }
 }
