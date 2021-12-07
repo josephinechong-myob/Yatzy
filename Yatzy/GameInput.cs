c37842a0373073c34887497a23f1ee1b6887c838
Josephine Chong <josephine.chong@myob.com>
2021-12-07 12:03:56 +1100

    tidied game input class

--- Yatzy/GameInput.cs
+++ Yatzy/GameInput.cs
@@ -15,75 +15,6 @@ namespace Yatzy
             _playerInputValidator = new PlayerInputValidator(console);
         }
         
-        public void PlayerChoosesCategory(Player player, GameDice _gameDice)
-        {
-            var chosenCategory = RequestPlayersCategory(player);
-            
-            if (chosenCategory == CategoryType.SpecificNumber)
-            { 
-                var specificNumber = RequestSpecificNumberType();
-                var specificNumberCategory = new Category(specificNumber, _gameDice.Dice);
-                player.ChooseCategory(specificNumberCategory);
-            }
-            else
-            {
-                var category = new Category(chosenCategory, _gameDice.Dice);
-                player.ChooseCategory(category);  
-            }
-        }
-        public SpecificNumberType RequestSpecificNumberType()
-        {
-            _console.WriteLine("Please enter a number from 1 to 6 which you want to use for your specific number");
-
-            var playerInput = _console.ReadLine();
-            while (!_playerInputValidator.IsOnlyNumbersOneToSix(playerInput))
-            {
-                _console.WriteLine("Please enter a number 1 to 6.");
-                playerInput = _console.ReadLine();
-            }
-            
-            var specificNumber = int.Parse(playerInput);
-            var number = (SpecificNumberType) specificNumber;
-            return number;
-        }
-        public CategoryType GetSelectedCategory(List<CategoryType> types)
-        {
-            var chosenCategory = _console.ReadLine(); 
-            while (!_playerInputValidator.IsOnlyNumbersOneToTen(chosenCategory))
-            {
-                _console.WriteLine("Please enter the number of the category you would like to select");
-            
-                chosenCategory = _console.ReadLine();
-            }
-           
-            var categoryIndex = int.Parse(chosenCategory);
-            var category = types.ElementAt(categoryIndex-1);
-            return category;
-        }
-        
-        public void PrintCategories(List<CategoryType> types)
-        {
-            
-            for (var i=0; i < types.Count(); i++)
-            {
-                var categoryNumber = i + 1;
-                _console.WriteLine($"[{categoryNumber}] - {types.ElementAt(i).ToString()}");
-            }
-        }
-        
-        public CategoryType RequestPlayersCategory(Player player) //testing in "synchronisatin'? // may need to 
-        {
-            _console.WriteLine($"Please select a category below:");
-            
-            var types = player.CategoryTypeRemaining;
-            PrintCategories(types);
-            
-            var category = GetSelectedCategory(types);
-            _console.WriteLine($"You have chosen {category} category");
-            
-            return category;
-        }
-        
         public bool AskIfPlayerWantsToHoldDice()
         {
             _console.WriteLine("Would you like to hold dice? Y - Yes, N - No");
@@ -112,6 +43,75 @@ namespace Yatzy
             return playerWantsToRollDice == "Y";
         }
         
+        private CategoryType GetSelectedCategory(List<CategoryType> types)
+        {
+            var chosenCategory = _console.ReadLine(); 
+            while (!_playerInputValidator.IsOnlyNumbersOneToTen(chosenCategory))
+            {
+                _console.WriteLine("Please enter the number of the category you would like to select");
+            
+                chosenCategory = _console.ReadLine();
+            }
+           
+            var categoryIndex = int.Parse(chosenCategory);
+            var category = types.ElementAt(categoryIndex-1);
+            return category;
+        }
         
+        private void PrintCategories(List<CategoryType> types)
+        {
+            
+            for (var i=0; i < types.Count(); i++)
+            {
+                var categoryNumber = i + 1;
+                _console.WriteLine($"[{categoryNumber}] - {types.ElementAt(i).ToString()}");
+            }
+        }
+        
+        private CategoryType RequestPlayersCategory(Player player)
+        {
+            _console.WriteLine($"Please select a category below:");
+            
+            var types = player.CategoryTypeRemaining;
+            PrintCategories(types);
+            
+            var category = GetSelectedCategory(types);
+            _console.WriteLine($"You have chosen {category} category");
+            
+            return category;
+        }
+        
+        private SpecificNumberType RequestSpecificNumberType()
+        {
+            _console.WriteLine("Please enter a number from 1 to 6 which you want to use for your specific number");
+
+            var playerInput = _console.ReadLine();
+            while (!_playerInputValidator.IsOnlyNumbersOneToSix(playerInput))
+            {
+                _console.WriteLine("Please enter a number 1 to 6.");
+                playerInput = _console.ReadLine();
+            }
+            
+            var specificNumber = int.Parse(playerInput);
+            var number = (SpecificNumberType) specificNumber;
+            return number;
+        }
+        
+        public void PlayerChoosesCategory(Player player, GameDice gameDice)
+        {
+            var chosenCategory = RequestPlayersCategory(player);
+            
+            if (chosenCategory == CategoryType.SpecificNumber)
+            { 
+                var specificNumber = RequestSpecificNumberType();
+                var specificNumberCategory = new Category(specificNumber, gameDice.Dice);
+                player.ChooseCategory(specificNumberCategory);
+            }
+            else
+            {
+                var category = new Category(chosenCategory, gameDice.Dice);
+                player.ChooseCategory(category);  
+            }
+        }
     }
 }
